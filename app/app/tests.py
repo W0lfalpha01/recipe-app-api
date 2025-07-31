f185924b4de149995a9fa6d8080df2c5ed20d67c
Steve-TLR <stefanms57@gmail.com>
2025-07-31 13:14:39 +0200

    fix Lint

--- app/app/tests.py
+++ app/app/tests.py
@@ -3,8 +3,8 @@ Sample Tests
 """
 
 from django.test import SimpleTestCase
-
 from app import calc
+
 
 class CalcTest(SimpleTestCase):
     """ Test the calc module """
@@ -12,12 +12,9 @@ class CalcTest(SimpleTestCase):
     def test_add_numbers(self):
         """ Test adding number together """
         res = calc.add(5, 6)
-
         self.assertEqual(res, 11)
-
 
     def test_subtract_numbers(self):
         """ Test subtracting number from together """
         res = calc.subtract(10, 15)
-
         self.assertEqual(res, 5)
