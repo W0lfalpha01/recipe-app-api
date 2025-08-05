ec41e3fe4abe4a7761c4f754d39a9684830ec63c
Steve-TLR <stefanms57@gmail.com>
2025-08-05 15:41:46 +0200

    recipe-details

---
+++ app/recipe/serializers.py
@@ -0,0 +1,27 @@
+"""
+Serializers for Recipe API's
+"""
+from rest_framework import serializers
+
+from core.models import Recipe
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """Serializer for Recipe objects"""
+
+    class Meta:
+        model = Recipe
+        fields = [
+            'id',
+            'title',
+            'time_minutes',
+            'price',
+            'link',
+        ]
+        read_only_fields = ['id']
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serializer for Recipe detail view"""
+
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ['description']
